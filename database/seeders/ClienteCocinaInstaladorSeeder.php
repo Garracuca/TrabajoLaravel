e7881a2ec49c04287cfa387c06c61a8df352bb07
Garracuca <mendez.genaro@yandex.com>
2024-02-18 22:07:52 +0100

    Update ClienteCocinaInstaladorSeeder.php

--- database/seeders/ClienteCocinaInstaladorSeeder.php
+++ database/seeders/ClienteCocinaInstaladorSeeder.php
@@ -17,31 +17,50 @@ class ClienteCocinaInstaladorSeeder extends Seeder
     public function run(): void
     {
         DB::table('cliente_cocina_instalador')->truncate();
-// hay que utilizar querybuilder porque no hay modelo propio
-// hay que USAR EL modelo de partner y book
-    $clientes= Cliente::InRandomOrder()->get(); //SOLO CAMPO ID    
-    $cocinas= Cocina::InRandomOrder()->get();
-    $instaladors= Instalador::InRandomOrder()->get();
+        $clientes = Cliente::inRandomOrder()->get(); //SOLO CAMPO ID    
+        $cocinas = Cocina::inRandomOrder()->get();
+        $instaladors = Instalador::inRandomOrder()->get();
+        $fechasUnicas = $this->fechasAleatorias(count($cocinas) * count($clientes) * count($instaladors));
 
-   foreach ($cocinas as $cocina){
-        foreach($clientes as $cliente){
-            foreach ($instaladors as $instalador) {
-                DB::table('cliente_cocina_instalador')->insert([
-                'fecha'=>now(),
-                'cliente_id'=>$cliente->id,
-                'cocina_id'=> $cocina->id,
-                'instalador_id' => $instalador->id,
-                'cantidad' => random_int(1,10),
-                'created_at'=>now(),
-                'updated_at'=>now(),
+        $index = 0;
 
-                ]);
+        foreach ($cocinas as $cocina) {
+            foreach ($clientes as $cliente) {
+                foreach ($instaladors as $instalador) {
+                    $fechaAleatoria = $fechasUnicas[$index++];
+                    DB::table('cliente_cocina_instalador')->insert([
+                        'fecha' => $fechaAleatoria,
+                        'cliente_id' => $cliente->id,
+                        'cocina_id' => $cocina->id,
+                        'instalador_id' => $instalador->id,
+                        'cantidad' => random_int(1, 10),
+                        'created_at' => now(),
+                        'updated_at' => now(),
+                    ]);
+                }
             }
+        }
+    }
 
+    private function fechasAleatorias(int $count): array
+    {
+        $dates = [];
+        $existingDates = DB::table('cliente_cocina_instalador')->pluck('fecha')->toArray();
+
+        while ($count > 0) {
+            $year = mt_rand(date('Y') - 5, date('Y'));
+            $month = mt_rand(1, 12);
+            $day = mt_rand(1, 28);
+
+            $fechaAleatoria = sprintf('%04d-%02d-%02d', $year, $month, $day);
+
+            if (!in_array($fechaAleatoria, $existingDates)) {
+                $dates[] = $fechaAleatoria;
+                $existingDates[] = $fechaAleatoria;
+                $count--;
+            }
         }
 
-
-
-   }
+        return $dates;
     }
 }
